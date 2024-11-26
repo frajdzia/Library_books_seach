743741fdbd9fac2b6ea3a824adaf7cbbdb1664a7
frajdzia <72212007+frajdzia@users.noreply.github.com>
2024-11-26 11:40:26 +0000

    deleted irreletant files

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
 //search bar
 import React from 'react';
 
-const SearchBar = (handleSearch) => {
+const SearchBar = ({handleSearch}) => {
     return(
         <input type="text" placeholder="Search books..."
         onChange={(e) => handleSearch(e.target.value)}/>
     );
-} ;
+};
 export default SearchBar;
